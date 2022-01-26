83f84794f0e9dfe53f6be38699c376d90181a617
baozoudashijian <Yhldcahcy521>
2022-01-26 17:11:10 +0800

    feat: shell脚本

---
+++ README.md
@@ -0,0 +1,9 @@
+## 开始使用
+
+### template.sh
+
+-将脚本文件放到相应的位置
+
+-cd到相应位置
+
+-执行sh template.sh
---
+++ src/demo.sh
@@ -0,0 +1,6 @@
+cd ~/DeskTop
+mkdir test
+cd ./test
+mkdir css js
+touch css/reset.css js/index.js index.html
+exit

0f628a8e8b45b7d407256a2349024ce22926494c
baozoudashijian <Yhldcahcy521>
2022-01-27 10:36:16 +0800

    fix: 注释内容

--- src/nodeScript.js
+++ src/nodeScript.js
@@ -13,8 +13,8 @@ fs.mkdirSync('./' + dirName) // mkdir $1
 process.chdir('./' + dirName) // cd $1
 fs.mkdirSync('css') // mkdir css
 fs.mkdirSync('js') // mkdir js
-fs.writeFileSync('index.html', '') // touch index.html
-fs.writeFileSync('css/style.css', '') // touche css/style.css
-fs.writeFileSync('js/main.js', '') // touch js/main.js
+fs.writeFileSync('index.html', '') // echo "" > index.html
+fs.writeFileSync('css/style.css', '') // echo  "" > css/style.css
+fs.writeFileSync('js/main.js', '') // echo ""  > js/main.js
 
 process.exit(0)
